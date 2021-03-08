e0b2fc910968cc759772b35f41a1cdc537c7973d
jaroslav <jaroslav.havrda@educanet.cz>
2021-03-08 23:57:57 +0100

    idkacko

---
+++ src/User.ts
@@ -0,0 +1,6 @@
+
+export interface User {
+  id: number;
+  jmeno: string;
+  heslo: string;
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { UsersComponent } from './users/users.component';
+import { UserComponent } from './user/user.component';
+import { HomeComponent } from './home/home.component';
+import {FormsModule} from '@angular/forms';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UsersComponent,
+    UserComponent,
+    HomeComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
